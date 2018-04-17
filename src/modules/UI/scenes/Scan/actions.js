b4ec23fe490485be0cf489eb7621581176b54882
Kevin Sullivan <kevin@airbitz.co>
2018-04-17 10:34:33 -0700

    WIP

--- src/modules/UI/scenes/Scan/actions.js
+++ src/modules/UI/scenes/Scan/actions.js
@@ -9,11 +9,10 @@ import * as WALLET_API from '../../../Core/Wallets/api.js'
 import * as UTILS from '../../../utils.js'
 import { loginWithEdge } from '../../../../actions/EdgeLoginActions.js'
 import { updateParsedURI } from '../../scenes/SendConfirmation/action.js'
-import s from '../../../../locales/strings.js'
 
 export const PREFIX = 'SCAN/'
 
-// SCENE
+// SCENE ////////////////////////////////////////////////////////////////
 export const SCENE_ENTERED = PREFIX + 'SCENE_ENTERED'
 export const sceneEntered = () => ({
   type: SCENE_ENTERED
@@ -24,29 +23,17 @@ export const sceneExited = () => ({
   type: SCENE_EXITED
 })
 
-export const RESET = PREFIX + 'RESET'
-export const reset = () => ({
-  type: RESET
+export const SCENE_ADDRESS_BUTTON_PRESSED = PREFIX + 'SCENE_ADDRESS_BUTTON_PRESSED'
+export const sceneAddressButtonPressed = () => ({
+  type: SCENE_ADDRESS_BUTTON_PRESSED
 })
 
-// CAMERA
-export const TORCH_BUTTON_PRESSED = PREFIX + 'TORCH_BUTTON_PRESSED'
-export const torchButtonPressed = () => ({
-  type: TORCH_BUTTON_PRESSED
+export const SCENE_TORCH_BUTTON_PRESSED = PREFIX + 'SCENE_TORCH_BUTTON_PRESSED'
+export const sceneTorchButtonPressed = () => ({
+  type: SCENE_TORCH_BUTTON_PRESSED
 })
 
-export const QR_CODE_SCANNED = PREFIX + 'QR_CODE_SCANNED'
-export const qrcodeScanned = (data: string) => ({
-  type: QR_CODE_SCANNED,
-  data: { data }
-})
-
-// ADDRESS_MODAL
-export const ADDRESS_BUTTON_PRESSED = PREFIX + 'ADDRESS_BUTTON_PRESSED'
-export const addressButtonPressed = () => ({
-  type: ADDRESS_BUTTON_PRESSED
-})
-
+// ADDRESS_MODAL ////////////////////////////////////////////////////////////////
 export const ADDRESS_MODAL_INPUT_CHANGED = PREFIX + 'ADDRESS_MODAL_INPUT_CHANGED'
 export const addressModalInputChanged = (data: string) => ({
   type: ADDRESS_MODAL_INPUT_CHANGED,
@@ -78,24 +65,7 @@ export const addressModalHidden = () => ({
   type: ADDRESS_MODAL_HIDDEN
 })
 
-// EDGE_LOGIN
-export const EDGE_LOGIN_REQUESTED = PREFIX + 'EDGE_LOGIN_REQUESTED'
-export const edgeLoginRequested = (uri: string) => ({
-  type: EDGE_LOGIN_REQUESTED,
-  data: { uri }
-})
-
-// LEGACY_ADDRESS_MODAL
-export const LEGACY_ADDRESS_DETECTED = PREFIX + 'LEGACY_ADDRESS_DETECTED'
-export const legacyAddressDetected = () => ({
-  type: LEGACY_ADDRESS_DETECTED
-})
-
-export const LEGACY_ADDRESS_MODAL_DEACTIVATED = PREFIX + 'LEGACY_ADDRESS_MODAL_DEACTIVATED'
-export const legacyAddressModalDeactivated = () => ({
-  type: LEGACY_ADDRESS_MODAL_DEACTIVATED
-})
-
+// LEGACY_ADDRESS_MODAL ////////////////////////////////////////////////////////////////
 export const LEGACY_ADDRESS_MODAL_CONFIRM_BUTTON_PRESSED = PREFIX + 'LEGACY_ADDRESS_MODAL_CONFIRM_BUTTON_PRESSED'
 export const legacyAddressModalConfirmButtonPressed = () => ({
   type: LEGACY_ADDRESS_MODAL_CONFIRM_BUTTON_PRESSED
@@ -121,25 +91,7 @@ export const legacyAddressModalHidden = () => ({
   type: LEGACY_ADDRESS_MODAL_HIDDEN
 })
 
-// TOKEN
-export const TOKEN_DETECTED = PREFIX + 'TOKEN_DETECTED'
-export const tokenDetected = () => ({
-  type: TOKEN_DETECTED
-})
-
-// OTHER
-export const PARSE_URI_SUCCEEDED = PREFIX + 'PARSE_URI_SUCCEEDED'
-export const parseUriSuceeded = (parsedUri: EdgeParsedUri) => ({
-  type: PARSE_URI_SUCCEEDED,
-  data: { parsedUri }
-})
-
-export const PARSE_URI_FAILED = PREFIX + 'PARSE_URI_FAILED'
-export const parseUriFailed = (error: Error) => ({
-  type: PARSE_URI_FAILED,
-  data: { error }
-})
-
+// INVALID_URI_MODAL /////////////////////////////////////////////////////////
 export const INVALID_URI_MODAL_BACKDROP_PRESSED = PREFIX + 'INVALID_URI_MODAL_BACKDROP_PRESSED'
 export const invalidUriModalBackdropPressed = () => ({
   type: INVALID_URI_MODAL_BACKDROP_PRESSED
@@ -155,41 +107,90 @@ export const invalidUriModalExpired = () => ({
   type: INVALID_URI_MODAL_EXPIRED
 })
 
+export const INVALID_URI_MODAL_HIDDEN = PREFIX + 'INVALID_URI_MODAL_HIDDEN'
+export const invalidUriModalHidden = () => ({
+  type: INVALID_URI_MODAL_HIDDEN
+})
 
+// CAMERA ////////////////////////////////////////////////////////////////
+export const CAMERA_QR_CODE_SCANNED = PREFIX + 'CAMERA_QR_CODE_SCANNED'
+export const cameraQrCodeScanned = (data: string) => ({
+  type: CAMERA_QR_CODE_SCANNED,
+  data: { data }
+})
 
-// OPERATIONS
+// PARSE ////////////////////////////////////////////////////////////////
+export const PARSE_URI_SUCCEEDED = PREFIX + 'PARSE_URI_SUCCEEDED'
+export const parseUriSuceeded = (parsedUri: EdgeParsedUri) => ({
+  type: PARSE_URI_SUCCEEDED,
+  data: { parsedUri }
+})
+
+export const PARSE_URI_FAILED = PREFIX + 'PARSE_URI_FAILED'
+export const parseUriFailed = (error: Error) => ({
+  type: PARSE_URI_FAILED,
+  data: { error }
+})
+
+// LEGACY_ADDRESS ////////////////////////////////////////////////////////////////
+export const LEGACY_ADDRESS_DETECTED = PREFIX + 'LEGACY_ADDRESS_DETECTED'
+export const legacyAddressDetected = (parsedUri: EdgeParsedUri) => ({
+  type: LEGACY_ADDRESS_DETECTED,
+  data: { parsedUri }
+})
+
+// EDGE_LOGIN ////////////////////////////////////////////////////////////////
+export const EDGE_LOGIN_DETECTED = PREFIX + 'EDGE_LOGIN_DETECTED'
+export const edgeLoginDetected = (uri: string) => ({
+  type: EDGE_LOGIN_DETECTED,
+  data: { uri }
+})
+
+// TOKEN ////////////////////////////////////////////////////////////////
+export const TOKEN_DETECTED = PREFIX + 'TOKEN_DETECTED'
+export const tokenDetected = (parameters: {}) => ({
+  type: TOKEN_DETECTED,
+  data: { parameters }
+})
+
+// PUBLIC_ADDRESS ////////////////////////////////////////////////////////////////
+export const PUBLIC_ADDRESS_DETECTED = PREFIX + 'PUBLIC_ADDRESS_DETECTED'
+export const publicAddressDetected = (parsedUri: EdgeParsedUri) => ({
+  type: PUBLIC_ADDRESS_DETECTED,
+  data: { parsedUri }
+})
+
+// OPERATIONS ////////////////////////////////////////////////////////////////
 export const qrcodeScanned = (data: string) => (dispatch: Dispatch, getState: GetState) => {
   const state = getState()
   if (!state.ui.scenes.scan.scanEnabled) return
 
-  // dispatch(disableScan())
-  // dispatch(qrcodeScanned(data))
+  dispatch(cameraQrCodeScanned(data))
 
   // EDGE LOGIN ///////////////////////////////////////////////////////////
   if (UTILS.isEdgeLogin(data)) {
+    dispatch(edgeLoginDetected(data))
     return dispatch(loginWithEdge(data))
   }
 
   const edgeWallet = state.core.wallets.byId[state.ui.wallets.selectedWalletId]
-  let parsedURI
+  let parsedUri
   try {
-    parsedURI = WALLET_API.parseURI(edgeWallet, data) // eslint-disable-line no-var
+    parsedUri = WALLET_API.parseURI(edgeWallet, data)
   } catch (error) {
     // INVALID QRCODE ///////////////////////////////////////////////////////
-    return Alert.alert(s.strings.fragment_send_send_bitcoin_unscannable, error.toString(), [
-      { text: s.strings.string_ok, onPress: () => dispatch(enableScan()) }
-    ])
+    return dispatch(parseUriFailed(error))
   }
   // TOKEN ////////////////////////////////////////////////////////////////
-  if (parsedURI.token) {
+  if (parsedUri.token) {
     // token URI, not pay
-    const { contractAddress, currencyName, multiplier } = parsedURI.token
-    const currencyCode = parsedURI.token.currencyCode.toUpperCase()
+    const { contractAddress, currencyName, multiplier } = parsedUri.token
+    const currencyCode = parsedUri.token.currencyCode.toUpperCase()
     const walletId = state.ui.wallets.selectedWalletId
     const wallet = state.ui.wallets.byId[walletId]
     let decimalPlaces = 18
-    if (parsedURI.token && parsedURI.token.multiplier) {
-      decimalPlaces = UTILS.denominationToDecimalPlaces(parsedURI.token.multiplier)
+    if (parsedUri.token && parsedUri.token.multiplier) {
+      decimalPlaces = UTILS.denominationToDecimalPlaces(parsedUri.token.multiplier)
     }
     const parameters = {
       contractAddress,
@@ -202,16 +203,17 @@ export const qrcodeScanned = (data: string) => (dispatch: Dispatch, getState: Ge
       onAddToken: UTILS.noOp
     }
 
+    dispatch(tokenDetected(parameters))
     return Actions.addToken(parameters)
   }
 
   // LEGACY ADDRESS ///////////////////////////////////////////////////////
-  if (parsedURI.legacyAddress) {
-    const currencyName = state.ui.settings[state.ui.wallets.selectedCurrencyCode].currencyCode
-    return dispatch(legacyAddressScanned(parsedURI, currencyName))
+  if (parsedUri.legacyAddress) {
+    return dispatch(legacyAddressDetected(parsedUri))
   }
 
   // PUBLIC ADDRESS ///////////////////////////////////////////////////////
-  dispatch(updateParsedURI(parsedURI))
+  dispatch(publicAddressDetected(parsedUri))
+  dispatch(updateParsedURI(parsedUri))
   return Actions.sendConfirmation('fromScan')
 }
